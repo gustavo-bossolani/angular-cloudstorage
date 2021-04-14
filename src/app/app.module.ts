d4d05810b48573d784fd8f9572bc75a56eb33ecb
Gustavo Bossolani <gustavo.wyd@outlook.com>
2021-04-14 00:42:31 -0300

    ajuste módulo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MaterialModule } from './material/material.module';
+import { MaterialModule } from './material.module';
 
 @NgModule({
   declarations: [
